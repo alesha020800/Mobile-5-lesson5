50303338826c9453746de5dd1f3d5a1ccad41384
alesha020800 <70952737+alesha020800@users.noreply.github.com>
2024-05-17 10:58:18 +0300

    Mobile

---
+++ audiorecord/src/main/java/ru/mirea/prokhorovad/audiorecord/MainActivity.java
@@ -0,0 +1,44 @@
+package ru.mirea.prokhorovad.audiorecord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_PERMISSION = 200;
+    private boolean isWork;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int audioRecordPermissionStatus = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.
+                WRITE_EXTERNAL_STORAGE);
+        if (audioRecordPermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus
+                == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+// Выполняется запрос к пользователь на получение необходимых разрешений
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECORD_AUDIO,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
+            grantResults) {
+// производится проверка полученного результата от пользователя на запрос разрешения Camera
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_CODE_PERMISSION:
+                isWork = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!isWork ) finish();
+    }
+}
